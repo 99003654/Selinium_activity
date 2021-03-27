01c4486522bc4a5e9fb322448f28ceac28daa0b9
99003654 <78997681+99003654@users.noreply.github.com>
2021-03-27 17:21:22 +0530

    Add files via upload

---
+++ activity/Demo.java
@@ -0,0 +1,65 @@
+	import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+public class Demo {
+
+
+
+
+		public static void main(String[] args) throws InterruptedException {
+			// TODO Auto-generated method stub
+			System.setProperty("webdriver.chrome.driver","/home/ltts/Documents/chromedriver");
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://www.facebook.com/campaign/landing.php?&campaign_id=973072061&extra_1=s%7Cc%7C231346502315%7Ce%7Cfacebook%7C&placement=&creative=231346502315&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D973072061%26adgroupid%3D54006406691%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-297690534863%26loc_physical_ms%3D1007773%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=EAIaIQobChMI0eK-pqbQ7wIVSQwrCh1dTgVOEAAYASAAEgLzcPD_BwE");
+//			driver.findElement(By.name("txtUsername")).sendKeys("Admin");
+//			driver.findElement(By.name("txtPassword")).sendKeys("admin123");
+//			driver.findElement(By.id("btnLogin")).click();
+////			driver.findElement(By.id("btnLogin")).click();
+//			driver.findElement(By.id("user-message")).sendKeys(" i am manzar");
+//			Thread.sleep(4000);
+//			driver.findElement(By.id("user-message")).clear();
+//			Thread.sleep(1000);
+////			driver.findElement(By.id("at-cv-lightbox-close")).click();
+////			driver.findElement(By.className("btn-default")).click()
+			driver.manage().window().maximize();
+//			driver.findElement(By.className("btn-default")).click();
+//			Thread.sleep(2000);
+//			driver.switchTo().alert().dismiss();
+//			Thread.sleep(4000);
+//			driver.findElement(By.className("btn-lg")).click();
+//			driver.switchTo().alert().dismiss();
+//			Select dd = new Select(driver.findElement(By.id("select-demo")));  
+//			dd.selectByVisibleText("Sunday");
+//			Thread.sleep(2000);
+//			dd.selectByValue("Friday");
+//			Thread.sleep(2000);
+//			Select d1 = new Select(driver.findElement(By.id("multi-select")));  
+//			d1.selectByValue("New Jersey");
+			driver.findElement(By.name("firstname")).sendKeys("manzar");
+			driver.findElement(By.name("lastname")).sendKeys("hussain");
+			driver.findElement(By.name("reg_email__")).sendKeys("manzar.hussain@gmail.com");
+		
+			driver.findElement(By.name("reg_email_confirmation__")).sendKeys("manzar.hussain@gmail.com");
+			driver.findElement(By.name("reg_passwd__")).sendKeys("manzar123");
+			
+			Select d1 = new Select(driver.findElement(By.id("day"))); 
+			d1.selectByValue("10");
+			Thread.sleep(2000);
+			Select d2 = new Select(driver.findElement(By.id("month"))); 
+			d2.selectByValue("7");
+			Select d3 = new Select(driver.findElement(By.id("year"))); 
+			d3.selectByValue("1998");
+			Thread.sleep(2000);
+			WebElement radio = driver.findElement(By.cssSelector("input[value='2']"));
+			radio.click();
+			Thread.sleep(2000);
+			driver.findElement(By.name("websubmit")).click();
+			
+			
+			
+			
+		}	
+
+}
